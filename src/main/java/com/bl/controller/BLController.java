60c739050fd4521eb04f7097f97547111170ba0f
zhanjian <zhanjian@ultrapower.com.cn>
2018-03-07 13:54:07 +0800

    修改SQL文件打印； 添加分页条件查询方法

--- src/main/java/com/bl/controller/BLController.java
+++ src/main/java/com/bl/controller/BLController.java
@@ -25,11 +25,35 @@ public class BLController {
     @Autowired
     private BlDao blDao;
 
+    /**
+     * 查询所有
+     * @return
+     */
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public List<BLEntity> findAll(){
         return blDao.findAll();
     }
 
+    /**
+     * 按分页查询
+     * @param pageNo
+     * @return
+     */
+    @RequestMapping(value = "/findAllByPage", method = RequestMethod.GET)
+    public Page<BLEntity> findAllByPage(int pageNo){
+        PageRequest pageRequest = PageRequest.of(pageNo, 2);
+
+        //获取分页内容：blDao.findAll(pageRequest).getContent()
+        //获取总条数：blDao.findAll(pageRequest).getTotalElements()
+        //获取总页数：blDao.findAll(pageRequest).getTotalPages()
+        return blDao.findAll(pageRequest);
+    }
+
+    /**
+     * 按条件分页查询
+     * @param pageNo
+     * @return
+     */
     @RequestMapping(value = "/findByPage", method = RequestMethod.GET)
     public Page<BLEntity> findByPage(int pageNo){
         PageRequest pageRequest = PageRequest.of(pageNo, 2);
@@ -37,11 +61,29 @@ public class BLController {
         //获取分页内容：blDao.findAll(pageRequest).getContent()
         //获取总条数：blDao.findAll(pageRequest).getTotalElements()
         //获取总页数：blDao.findAll(pageRequest).getTotalPages()
+        BLEntity blEntity = new BLEntity();
+        blEntity.setName("3");
+        Example<BLEntity> blEntitys = Example.of(blEntity);
+        return blDao.findAll(blEntitys, pageRequest);
+    }
 
-//        Example<BLEntity> blEntity = Example.of();
-//        blEntity.setName("3");
+    /**
+     * 新增
+     * @return
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public void add(){
+        BLEntity blEntity = new BLEntity();
+        blEntity.setName("3");
+        blDao.save(blEntity);
+    }
 
-        return blDao.findAll(pageRequest);
-
+    /**
+     * 删除
+     * @return
+     */
+    @RequestMapping(value = "/del", method = RequestMethod.GET)
+    public void del(String id){
+        blDao.deleteById(Long.parseLong(id));
     }
 }
